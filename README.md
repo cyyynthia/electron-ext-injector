d365b49550404723be060bb9ed7b9de59cd06849
Cynthia <cynthia@cynthia.dev>
2022-06-10 23:54:31 +0200

    feat: add popups n options screens

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Electron extension injector
 injects Chromium extension into packaged electron apps. highly experimental. might work.
 
+use Alt+Shift+E to access extension menus & settings & stuff.
+
+many extensions won't work because of electron's limited apis. deal with it.
+
 notes:
  - may not like updates and require re-injecting. deal with it. make shell scripts. :shrug:
  - doesn't deal with updates. maybe if it ends up bothering me ill add it. make shell scripts. :shrug:
@@ -27,3 +31,14 @@ no
 
 ## License
 Copyright (c) 2022 Cynthia K. Rey, All rights reserved.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--- src/eei-loader.cjs
+++ src/eei-loader.cjs
@@ -3,12 +3,67 @@
 const fs = require('fs')
 const path = require('path')
 const electron = require('electron')
+const extensionsHtml = path.join(__dirname, 'extensions.html')
 
 electron.app.on('ready', async () => {
   const extPath = path.join(require.main.path, '..', '..', 'eei-exts')
-  console.log(await Promise.all(
-    fs.readdirSync(extPath).map(
-      (ext) => electron.session.defaultSession.loadExtension(path.join(extPath, ext))
-    )
-  ))
+  for (const ext of fs.readdirSync(extPath)) {
+    await electron.session.defaultSession.loadExtension(path.join(extPath, ext))
+  }
+})
+
+let optWin
+let view
+
+function onResize (e) {
+  if (!view) return
+  const { width: w } = e.sender.getBounds()
+  const { width, height, y } = view.getBounds()
+  const x = w - width - 24
+
+  view.setBounds({ x, y, width, height })
+}
+
+electron.app.on('web-contents-created', (_, wc) => {
+  wc.on('before-input-event', async (event, input) => {
+    if (input.alt && input.shift && input.code === 'KeyE' && !input.ctrl && !input.meta) {
+      event.preventDefault()
+      if (view) return
+
+      view = new electron.BrowserView()
+      const win = electron.BrowserWindow.getFocusedWindow()
+      const clickPromise = win.webContents.executeJavaScript('new Promise((r) => document.body.addEventListener("click", (e) => e.preventDefault()|r(), { capture: true, once: true }))')
+      const styleId = await win.webContents.insertCSS('body * { pointer-events: none }')
+
+      const extensions = electron.session.defaultSession.getAllExtensions().map((e) => ({
+        id: e.id,
+        name: e.name,
+        baseUrl: e.url,
+        popup: e.manifest.browser_action?.default_popup || e.manifest.action?.default_popup || null,
+        options: e.manifest.options_ui?.page || e.manifest.options_page || null,
+      }))
+
+      clickPromise.then(() => {
+        win.off('resize', onResize)
+        win.removeBrowserView(view)
+        win.webContents.removeInsertedCSS(styleId)
+        view = null
+      })
+
+      win.on('resize', onResize)
+      view.setBounds({ x: 0, y: 24, width: 360, height: 480 })
+      view.webContents.loadURL(`file://${extensionsHtml}`)
+      win.addBrowserView(view)
+      onResize({ sender: win })
+
+      const action = await view.webContents.executeJavaScript(`window._run(${JSON.stringify(extensions)})`)
+      if (action.action === 'options') {
+        win.webContents.executeJavaScript('document.body.click()')
+        optWin = new electron.BrowserWindow()
+        optWin.loadURL(action.dst)
+      } else {
+        view.webContents.loadURL(action.dst)
+      }
+    }
+  })
 })
